7ec90d5e57618ba1091a771e2198d7415d12dc22
Iwan de Jong <idj.idejong@gmail.com>
2023-11-01 18:04:43 +0200

    extended testing (failing)

--- main.cpp
+++ main.cpp
@@ -3,7 +3,12 @@
 int main() {
     Facade* facade = new Facade();
     facade->getSeated();
+    facade->getSeated();
+    facade->getSeated();
+    facade->getSeated();
+    facade->getSeated();
+    facade->getSeated();
     facade->addToOrder(0, "Steak");
-    facade->addToOrder(1, "Steak");
+    facade->addToOrder(12, "Steak");
     return 0;
 }
