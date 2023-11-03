c0a0d50f98cf51e9e1048a4d3e33621de43f6d61
Iwan de Jong <idj.idejong@gmail.com>
2023-11-03 13:42:09 +0200

    change header description

--- TableObserver.h
+++ TableObserver.h
@@ -1,5 +1,5 @@
-#ifndef TABLEOBSERVER_H
-#define TABLEOBSERVER_H
+#ifndef TABLE_OBSERVER_H
+#define TABLE_OBSERVER_H
 
 // ConcreteObserver (Observer)
 
@@ -18,4 +18,4 @@ class TableObserver {
         ~TableObserver();
         void update(std::string item, bool isItem);
 };
-#endif // TABLEOBSERVER_H
+#endif // TABLE_OBSERVER_H
