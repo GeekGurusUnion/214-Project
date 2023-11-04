50fcbfaafc1ed9d4fb5094aebf961328412c7aba
Iwan de Jong <idj.idejong@gmail.com>
2023-11-04 11:39:59 +0200

    added basic unit test (working)

--- unitTests_Iwan.cpp
+++ unitTests_Iwan.cpp
@@ -7,25 +7,30 @@
 // #include class to best tested
 #include "Waiter.h"
 
-// // make a MOCK class to replace the class that is being tested
-// class MockWaiter : public Waiter {
-//     public:
-//         MockWaiter(std::string name, int totalOrders, FloorColleague *fc) : Waiter(name, totalOrders, fc) {}
-//         MOCK_METHOD(void, setWaiterState, (WaiterState* state), (override));
-//         MOCK_METHOD(void, addItem, (RestaurantTable* rt, std::string m), (override));
-//         MOCK_METHOD(void, confirmOrder, (RestaurantTable* rt), (override));
-//         MOCK_METHOD(void, addOrder, (RestaurantTable* rt), (override));
-//         MOCK_METHOD(Order*, getOrder, (RestaurantTable* rt), (override));
-//         MOCK_METHOD(bool, isAvailable, (), (override));
-//         MOCK_METHOD(std::string, getName, (), (override));
-//         MOCK_METHOD(MenuItem*, getMenuItem, (std::string name), (override));
-//         MOCK_METHOD(int, getBusyOrders, (), (override));
-// };
-
 // #include all relative classes needed for successful testing
 
-//our first unit test
+// make a MOCK class to replace the class that is being tested
+class MockWaiter : public Waiter {
+    public:
+        MockWaiter(std::string name, int totalOrders, FloorColleague *fc) : Waiter(name, totalOrders, fc) {}
+        MOCK_METHOD(void, setWaiterState, (WaiterState* state));
+        MOCK_METHOD(void, addItem, (RestaurantTable* rt, std::string m));
+        MOCK_METHOD(void, confirmOrder, (RestaurantTable* rt));
+        MOCK_METHOD(void, addOrder, (RestaurantTable* rt));
+        MOCK_METHOD(Order*, getOrder, (RestaurantTable* rt));
+        MOCK_METHOD(bool, isAvailable, ());
+        MOCK_METHOD(std::string, getName, ());
+        MOCK_METHOD(MenuItem*, getMenuItem, (std::string name));
+        MOCK_METHOD(int, getBusyOrders, ());
+};
 
+// test waiter
+TEST(WaiterTest, waiterStateSet) {
+    Waiter *w = new Waiter("Bob", 5, nullptr);
+    WaiterState *ws = new WaiterStateAvailable(w);
+    w->setWaiterState(ws);
+    EXPECT_EQ(w->isAvailable(), true) << "Waiter state not set correctly";
+}
 
 
 int main(int argc, char **argv) {
