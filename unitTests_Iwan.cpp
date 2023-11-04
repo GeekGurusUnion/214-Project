246bb09f0ee2ca937207a2baca27c06360bea085
Iwan de Jong <idj.idejong@gmail.com>
2023-11-04 11:16:39 +0200

    update unittest_iwan

--- unitTests_Iwan.cpp
+++ unitTests_Iwan.cpp
@@ -2,31 +2,36 @@
 
 //include the google test dependencies
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
-//declare the function(s) that you are testing
-double summer(double[], int);
+// #include class to best tested
+#include "Waiter.h"
+
+#ifndef 
+#error You need to include math.h 
+#endif
+
+// make a MOCK class to replace the class that is being tested
+class MockWaiter : public Waiter {
+    public:
+        MockWaiter(std::string name, int totalOrders, FloorColleague *fc) : Waiter(name, totalOrders, fc) {}
+        MOCK_METHOD(void, setWaiterState, (WaiterState* state), (override));
+        MOCK_METHOD(void, addItem, (RestaurantTable* rt, std::string m), (override));
+        MOCK_METHOD(void, confirmOrder, (RestaurantTable* rt), (override));
+        MOCK_METHOD(void, addOrder, (RestaurantTable* rt), (override));
+        MOCK_METHOD(Order*, getOrder, (RestaurantTable* rt), (override));
+        MOCK_METHOD(bool, isAvailable, (), (override));
+        MOCK_METHOD(std::string, getName, (), (override));
+        MOCK_METHOD(MenuItem*, getMenuItem, (std::string name), (override));
+        MOCK_METHOD(int, getBusyOrders, (), (override));
+};
+
+// #include all relative classes needed for successful testing
 
 //our first unit test
-TEST(IntegerInputsSuite, simpleSum)
-{
-  //first, set up any inputs to your 
-  const int SIZE = 3;
-  double arr[SIZE]  = {1, 2, 3};
-  //then, make an assertion to test
-  EXPECT_EQ(summer(arr, SIZE), 6) << "The sum is not correct";
-}
-TEST(IntegerInputsSuite, oneElement)
-{
-  const int SIZE = 1;
-  double arr[SIZE]  = {33};
-  EXPECT_EQ(summer(arr, SIZE), 33) << "The sum is not correct for array of size 1";
-}
-TEST(DoubleInputsSuite, simpleSum)
-{
-  const int SIZE = 3;
-  double arr[SIZE]  = {1.1, 1.1, 1};
-  EXPECT_EQ(summer(arr, SIZE), 3.2) << "The sum is not correct using     double inputs";
-}
+
+
+
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
