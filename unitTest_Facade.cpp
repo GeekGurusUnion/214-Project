2e59a0886fa5e0ece966b469946170fe7873e4aa
Iwan de Jong <idj.idejong@gmail.com>
2023-11-04 17:29:42 +0200

    added Facade test

---
+++ unitTest_Facade.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <memory>
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "Facade.h"
+
+class MockFacade : public Facade {
+    public:        
+        MockFacade() : Facade() {} // Facade();
+        MOCK_METHOD0(getSeated, void());  // void getSeated();
+        MOCK_METHOD2(addToOrder, void(int, std::string)); // void addToOrder(int tableNumber, std::string itemName);
+        MOCK_METHOD1(confirmOrder, void(int)); // void confirmOrder(int tableNumber);
+        MOCK_METHOD1(leaveTable, void(int)); // void leaveTable(int tableNumber);
+        MOCK_METHOD0(getWaiterIterator, Iterator*());  // WaiterIterator* createWaiterIterator();
+        MOCK_METHOD0(getTableIterator, Iterator*()); // TableIterator* createTableIterator();
+        MOCK_METHOD1(getTable, RestaurantTable*(int)); // RestaurantTable* getTable(int index);
+        MOCK_CONST_METHOD0(getWaiterSize, int()); // int getWaiterSize() const;
+        MOCK_CONST_METHOD0(getTablesPerWaiter, int()); // int getTablesPerWaiter() const;
+        MOCK_CONST_METHOD0(getTotalTables, int()); // int getTotalTables() const;
+        MOCK_METHOD0(createTableIterator, TableIterator*()); // Iterator* getWaiterIterator();
+        MOCK_METHOD0(createWaiterIterator, WaiterIterator*()); // Iterator* getTableIterator();
+        MOCK_METHOD1(getMenuItem, MenuItem*(std::string)); // MenuItem* getMenuItem(std::string name);
+        MOCK_METHOD1(getWaiter, void(RestaurantTable*)); // void getWaiter(RestaurantTable* table);
+        MOCK_METHOD1(addTable, void(RestaurantTable*)); // void addTable(RestaurantTable* table);
+        MOCK_METHOD1(addWaiter, void(Waiter*)); // void addWaiter(Waiter* waiter);
+};
+
+// TODO: Test extensively here (replaces main.cpp) such as control flows and exceptions
+
+// * Test if all the members are properly initialized in the Facade class
+TEST(FacadeConstructor, Constructor) {
+    MockFacade facade();
+    EXPECT_EQ(facade.getWaiterSize(), 4);
+    EXPECT_EQ(facade.getTablesPerWaiter(), 3);
+    EXPECT_EQ(facade.getTotalTables(), 12);
+}
