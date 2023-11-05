a834ec20677c995560a2d29c1bd7cdf439de17c5
Leviathan6331 <106149065+Leviathan6331@users.noreply.github.com>
2023-11-05 22:32:29 +0200

    Resolution

---
+++ kitchen/headChefTest.cpp
@@ -0,0 +1,27 @@
+#include <stdexcept>
+#include "headChefTest.h"
+
+std::string testHeadChef(const std::string &item, const std::vector<std::string> &customizations){
+    ad_RestaurantOrder* order = new ad_RestaurantOrder(item, customizations);
+
+    si_headChef* chef = si_headChef::getInstance();
+
+    fa_dish* dish = chef->prepareDish(*order);
+
+    std::string result;
+
+    if (dish != nullptr){
+        result = dish->getDescription();
+    }else{
+        result = "Dish unavailable.";
+    }
+
+    delete order;
+    delete dish;
+
+    return result;
+}
+
+si_headChef* testSingleton(){
+    return si_headChef::getInstance();
+}
