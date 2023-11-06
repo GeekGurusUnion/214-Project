e76cff968b2a23ddcd4bf6c38de56fee6cd572fc
SW1F7YY <stephansmit112@gmail.com>
2023-11-06 10:40:43 +0200

    Added Doxy tags to files Colleague.h -> Order.h

--- Colleague.h
+++ Colleague.h
@@ -1,20 +1,53 @@
 #ifndef COLLEAGUE_H
 #define COLLEAGUE_H
-
 // Colleague (Mediator) [interface]
+/**
+ * @file Colleague.h
+ * @brief Declaration of the Colleague class, an interface for Mediator pattern.
+ */
 
+// Forward declarations
 class Mediator;
 class Order;
 
+/**
+ * @class Colleague
+ * @brief Abstract base class for colleagues in the Mediator pattern.
+ *
+ * Colleague represents a participant in the Mediator pattern, which collaborates with other colleagues
+ * through a mediator to achieve a common goal.
+ */
 class Colleague {
-    protected:
-        Mediator* mediator;
-        Order* order;
-    public:
-        Colleague(Mediator* mediator);
-        virtual void changed();
-        void setOrder(Order* order);
-        Order* getOrder();
+protected:
+    Mediator* mediator; ///< A pointer to the Mediator that facilitates communication.
+    Order* order;       ///< A pointer to the Order associated with this Colleague.
+
+public:
+    /**
+     * @brief Constructor for the Colleague class.
+     * @param mediator A pointer to the Mediator used for communication with other colleagues.
+     */
+    Colleague(Mediator* mediator);
+
+    /**
+     * @brief Notify the Mediator that this colleague has changed state or data.
+     *
+     * This method is called when the state or data of the colleague has changed and needs to inform
+     * the Mediator.
+     */
+    virtual void changed();
+
+    /**
+     * @brief Set the Order associated with this Colleague.
+     * @param order A pointer to the Order to be associated with this colleague.
+     */
+    void setOrder(Order* order);
+
+    /**
+     * @brief Get the Order associated with this Colleague.
+     * @return A pointer to the Order associated with this colleague.
+     */
+    Order* getOrder();
 };
 
 #endif // COLLEAGUE_H
--- CustomizeOrder.h
+++ CustomizeOrder.h
@@ -1,16 +1,46 @@
 #ifndef CUSTOMIZE_ORDER_H
 #define CUSTOMIZE_ORDER_H
-
 // ConcreteCommand (Command)
+/**
+ * @file CustomizeOrder.h
+ * @brief Declaration of the CustomizeOrder class, a concrete implementation of the Command pattern.
+ */
 
 #include "Command.h"
 class Waiter;
+class RestaurantTable;
+class MenuItem;
 
+/**
+ * @class CustomizeOrder
+ * @brief A concrete command for customizing an order with specific customization details.
+ *
+ * CustomizeOrder is a specific command that represents the action of customizing an order at a restaurant.
+ * It is associated with a particular Waiter and allows customization of a given RestaurantTable's order.
+ */
 class CustomizeOrder : public Command {
-    public:
-        CustomizeOrder(Waiter* waiter);
-        void execute(RestaurantTable* rt, std::string m, std::string customization);
-        void execute(RestaurantTable* rt, MenuItem* m) {}
+public:
+    /**
+     * @brief Constructor for the CustomizeOrder class.
+     * @param waiter A pointer to the Waiter issuing the customization command.
+     */
+    CustomizeOrder(Waiter* waiter);
+
+    /**
+     * @brief Execute the customization of an order with specific customization details.
+     * @param rt A pointer to the RestaurantTable for which the order is being customized.
+     * @param m A string representing the name of the MenuItem being customized.
+     * @param customization A string specifying the customization details.
+     */
+    void execute(RestaurantTable* rt, std::string m, std::string customization);
+
+    /**
+     * @brief Dummy execution method for customization with MenuItem (not used).
+     * @param rt A pointer to the RestaurantTable for which the order is being customized.
+     * @param m A pointer to the MenuItem (not used in this method).
+     */
+    void execute(RestaurantTable* rt, MenuItem* m) {}
 };
 
 #endif // CUSTOMIZE_ORDER_H
+
