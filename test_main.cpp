4a852089f333cd30f51c1d27140ec3a42189b43b
Iwan de Jong <idj.idejong@gmail.com>
2023-11-06 09:37:49 +0200

    commented kitchen path

--- test_main.cpp
+++ test_main.cpp
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
-#include "kitchen/factory_unittest.cpp"
-#include "kitchen/headChef_unittest.cpp"
+// #include "kitchen/factory_unittest.cpp"
+// #include "kitchen/headChef_unittest.cpp"
 #include <gtest/gtest.h>
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
-=======
-#include "kitchen/factory_unittest.cpp"
-#include "kitchen/headChef_unittest.cpp"
-#include <gtest/gtest.h>
-
-int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
->>>>>>> 7445f2bc56b97a1dd23166483edccaa3f44c8ce9
 }
